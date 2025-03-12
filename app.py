427002f29e83b985163b0d9502030edba0cbccb9
Andre Menezes <djdedemenezes@gmail.com>
2025-03-12 14:38:05 -0300

    refac passengers toslider and fix date time format

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ import pandas as pd
 '''
 # TaxiFareModel front
 '''
-url = 'https://taxifarededev-768760105976.europe-west1.run.app/predict'
+url = 'https://taxifare.lewagon.ai/predict'
 
 with st.form("my_form"):
 
     pickup_date = st.date_input(
-        "When's your ride?"
+        "When's your ride?",
+        value='2014-03-12'
     )
     pickup_time = st.time_input('Set pickup time:', step=300)
 
@@ -43,16 +44,16 @@ with st.form("my_form"):
         step=0.000001,
         value=40.769802
     )
-    passengers = st.number_input(
-        'Number of passengers',
-        placeholder='Number of passengers',
+    passengers = st.slider('Passengers',
+        min_value=1,
+        max_value=4,
+        value=2,
         step=1,
-        value=2
     )
     submitted = st.form_submit_button("Submit")
     if submitted:
         params = dict(
-            pickup_datetime=datetime.datetime.combine(pickup_date, pickup_time).strftime('%Y/%m/%d %H:%M:%S'),
+            pickup_datetime=datetime.datetime.combine(pickup_date, pickup_time).strftime('%Y-%m-%d %H:%M:%S'),
             pickup_longitude=pickup_longitude,
             pickup_latitude=pickup_latitude,
             dropoff_longitude=dropoff_longitude,
@@ -64,17 +65,18 @@ if submitted:
     endpoint_url = url + f"?{''.join('{}={}&'.format(key, val) for key, val in params.items())}"
     response = requests.get(endpoint_url)
     f'''
-    Submitted parameters:
     '''
+    st.markdown("### Submitted parameters:")
     st.write(params)
-    f'''
-    Predicted price: **${round(response.json()['fare'], 2)}**
-    '''
+    st.markdown(f"### Predicted price: `${round(response.json()['fare'], 2)}`")
     df = pd.DataFrame([
-        {"latitude": params["pickup_latitude"], "longitude": params["pickup_longitude"]},
-        {"latitude": params["dropoff_latitude"], "longitude": params["dropoff_longitude"]}
+        {"color": '#65d2d9',"latitude": params["pickup_latitude"], "longitude": params["pickup_longitude"]},
+        {"color": '#5fd76a',"latitude": params["dropoff_latitude"], "longitude": params["dropoff_longitude"]}
     ])
-    st.write(df)
-    st.map(df)
+    st.map(df, color='color')
 else:
-    st.map()
+    df = pd.DataFrame([
+        {"color": '#65d2d9', "latitude": pickup_latitude, "longitude": pickup_longitude},
+        {"color": '#5fd76a', "latitude": dropoff_latitude, "longitude": dropoff_longitude}
+    ])
+    st.map(df, color='color')
